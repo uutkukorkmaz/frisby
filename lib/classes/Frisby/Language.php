72595c8d201f7e85b931e8bbbc38d935e0a5b985
uutkukorkmaz <uutkukorkmaz@gmail.com>
2020-03-20 13:18:26 +0300

    Multilingual optimizations

--- lib/classes/Frisby/Language.php
+++ lib/classes/Frisby/Language.php
@@ -49,9 +49,51 @@ class Language
 
 	}
 
+	/**
+	 * @param $str
+	 * @return mixed
+	 */
 	public function __($str)
 	{
 		return isset($GLOBALS['language'][$str]) ? $GLOBALS['language'][$str] : $str;
+	}
+
+	/**
+	 * @return array
+	 */
+	public static function getAvailableLangs(){
+		$dir = realpath('.').'\lib\languages';
+		$res = [];
+		if ($handle = opendir($dir)) {
+			while ($file = readdir($handle)) {
+				if (!is_dir($file)) {
+					$res[] = explode('.',$file)[0];
+				}
+			}
+		}
+		asort($res,SORT_ASC);
+		$result = [];
+		foreach($res as $re){
+			$result[] = $re;
+		}
+		return $result;
+	}
+
+	/**
+	 * Generates link tags for Multilingual SEO
+	 * @return string
+	 */
+	public static function generateHrefLang(){
+		global $app,$lang;
+		$html = "";
+		foreach (self::getAvailableLangs() as $language) {
+			if($language != $lang->getDefaultLang()) {
+				$html .= '<link rel="alternate" href="' . $app->go($language) . '" hreflang="' . $language . '">' . PHP_EOL;
+			}else{
+				$html .= '<link rel="alternate" href="' . $app->go() . '" hreflang="' . $language . '">' . PHP_EOL;
+			}
+		}
+		return $html;
 	}
 
 
